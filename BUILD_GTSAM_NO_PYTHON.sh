957c09704f2983c47c281dacf00aac06415d1da9
PeterC <petercalifano.gs@gmail.com>
2024-05-19 10:45:27 +0200

    Modification to support Ubuntu 24.04TLS

--- BUILD_GTSAM_NO_PYTHON.sh
+++ BUILD_GTSAM_NO_PYTHON.sh
@@ -4,7 +4,8 @@
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install libboost-all-dev cmake libtbb-dev -y
+sudo apt-get install libboost1.74-all-dev cmake libtbb-dev -y
+#sudo apt-get install libboost-all-dev cmake libtbb-dev -y
 
 # NEED TO ADD IF "not installed" for the following:
 sudo apt-get install python3-pip -y 
@@ -14,24 +15,24 @@ sudo apt-get install libeigen3-dev -y
 
 
 buildpath="build"
-if ! [ -d $buildpath ]; then
-    mkdir build
-    cd build
-else
-    echo "build directory exists, skipping creation, cleaning... "
-    cd build 
-    make clean
+if  [ -d $buildpath ]; then
+    sudo rm -r build/
 fi
 
 # ADD check to see if in build else throw error
+export CC=/usr/bin/gcc-11
+export CXX=/usr/bin/g++-11
 
 # Generate makefiles with verbose output log
-cmake   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake  -B build -S . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DGTSAM_BUILD_UNSTABLE:OPTION=OFF -DGTSAM_BUILD_CONVENIENCE_LIBRARIES:OPTION=ON \
         -DGTSAM_BUILD_PYTHON=OFF -DGTSAM_INSTALL_MATLAB_TOOLBOX=ON \
         -DGTSAM_WITH_TBB=ON \
-        -DGTSAM_WITH_EIGEN_MKL=OFF 
+        -DGTSAM_WITH_EIGEN_MKL=OFF\
+	-DGTSAM_UNSTABLE_BUILD_PYTHON=OFF 
 #        -DCMAKE_VERBOSE_MAKEFILE=ON . |& tee CMAKE_OUTPUT_LOG.txt .. 
+
+cd build
 
 # Build and install (system-wide)
 sudo make check -j4 # |& tee MAKE_CHECK_OUTPUT_LOG.txt
