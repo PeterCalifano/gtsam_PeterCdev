d1915f8336d02249c215413d4ca9374f69bf3788
PeterC <petercalifano.gs@gmail.com>
2025-08-01 13:13:22 +0200

    Update build scripts

--- BUILD_GTSAM.sh
+++ BUILD_GTSAM.sh
@@ -5,15 +5,16 @@
 # Last updated with shell parser by PeterC, July 2024
 
 # NOTE: If -r script is only allowed to change CXX flags and optionally include wrappers.
+set -e # Exit on error
+set -o pipefail # Exit on error in a pipeline
 
 # Default values
-Buildpath="build" 
-IS_BUILDPATH_DEFAULT=true
+build_path=build
+install_path=install
+is_default_build_path=true
 jobs=3
-WITH_DYNAMICS_MODULE=false
-install_deps=false
 rebuild=false
-BUILD_TYPE=debug # default build type, possible options: debug, release, relwithdebinfo, minsizerel
+BUILD_TYPE=relwithdebinfo # default build type, possible options: debug, release, relwithdebinfo, minsizerel
 ADD_CHECKS=false
 ADD_CXX_FLAGS=""
 WITH_PYTHON=false
@@ -25,8 +26,8 @@ PYTHON_EXE=$HOME/miniconda3/home/peterc/miniconda3/envs/gtsam/bin/python3.12 # D
 
 # Parse options using getopt
 # NOTE: no ":" after option means no argument, ":" means required argument, "::" means optional argument
-OPTIONS=B::,j::,i,r,t::,c,f::,p::,m::,u,e,o
-LONGOPTIONS=Buildpath::,jobs::,install-deps,rebuild,type-build::,checks,flagsCXX::,python-wrap::,matlab-wrap::,unstable_build,exp_map_enabled,on_manifold_preintegr
+OPTIONS=B::,j::,i::,r,t::,c,f::,p::,m::,u,e,o
+LONGOPTIONS=build_path::,jobs::,install-path::,rebuild,type-build::,checks,flagsCXX::,python-wrap::,matlab-wrap::,unstable_build,exp_map_enabled,on_manifold_preintegr
 
 # Parsed arguments list with getopt
 PARSED=$(getopt --options ${OPTIONS} --longoptions ${LONGOPTIONS} --name "$0" -- "$@")
@@ -44,15 +45,15 @@ eval set -- "$PARSED"
 # Process options (change default values if needed)
 while true; do
   case "$1" in
-    -B|--Buildpath)
+    -B|--build_path)
       if [ -n "$2" ] && [ "$2" != "--" ]; then # Check how many args (if 2)
-        Buildpath="$2"
-        IS_BUILDPATH_DEFAULT=false
+        build_path="$2"
+        is_default_build_path=false
         shift 2 # Shift of two args, i.e. $1 will then point to the next argument
       else 
       # Handle the default case (no optional argument provided), thus shift of 1
-        Buildpath="build"
-        IS_BUILDPATH_DEFAULT=true
+        build_path="build"
+        is_default_build_path=true
         shift
       fi
       ;;
@@ -65,9 +66,9 @@ while true; do
         shift
       fi
       ;;
-    -i|--install-deps)
-      install_deps=true
-      shift
+    -i|--install-path)
+      install_path="$2"
+      shift 2
       ;;
     -r|--rebuild)
       rebuild=true
@@ -142,52 +143,42 @@ done
 
 if [ "${rebuild}" = true ]; then
   # REBUILDING FROM EXISTING BUILD
-  if [ "${IS_BUILDPATH_DEFAULT}" = true ]; then
-    Buildpath="build_dev" # Set default for rebuild if not specified
+  if [ "${is_default_build_path}" = true ]; then
+    build_path="build_dev" # Set default for rebuild if not specified
   fi   
   echo "REBUILDING GTSAM with options..."
-  echo -e "\tBuildpath: $Buildpath"
+  echo -e "\tbuild_path: $build_path"
   echo -e "\tJobs: $jobs"
   echo -e "\tBuild Type: ${BUILD_TYPE}"
   echo -e "\tEnforced compile flags: ${ADD_CXX_FLAGS}"
   echo -e "\tPython wrapper build: ${WITH_PYTHON}"
   echo -e "\tPython executable path: ${PYTHON_EXE}"
   echo -e "\tMATLAB wrapper build: ${WITH_MATLAB}"
-  echo -e "\tWith Dynamics Module: ${WITH_DYNAMICS_MODULE}"
   echo -e "\tBuild GTSAM unstable: ${WITH_UNSTABLE}"
-  if ! [ -d $Buildpath ]; then
+  if ! [ -d $build_path ]; then
       echo "ERROR: NO PREVIOUS BUILD FOUND! EXITING..." >&2
       exit 1
   else
 
   sleep 1
 
-  #cmake  -B ${Buildpath} -S . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  #        -DGTSAM_BUILD_UNSTABLE:OPTION=OFF -DGTSAM_BUILD_CONVENIENCE_LIBRARIES:OPTION=ON \
-  #        -DGTSAM_BUILD_PYTHON=${WITH_PYTHON} -DGTSAM_INSTALL_MATLAB_TOOLBOX=${WITH_MATLAB} \
-  #        -DGTSAM_WITH_TBB=ON \
-  #        -DGTSAM_WITH_EIGEN_MKL=OFF \
-  #	       -DGTSAM_UNSTABLE_BUILD_PYTHON=${WITH_PYTHON} \
-  #        -DGTSAM_WITH_DYNAMICS=${WITH_DYNAMICS_MODULE} \
-  #        -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} \
-  #        -DCMAKE_C_FLAGS=${ADD_CXX_FLAGS}
-
   # Build and install (system-wide)
   if [ ${BUILD_TYPE} == "debug" ]; then
-    sudo cmake ${Buildpath} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} \
+    cmake ${build_path} -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} \
     -DCMAKE_C_FLAGS=${ADD_CXX_FLAGS} \
     -DGTSAM_BUILD_PYTHON=${WITH_PYTHON} \
     -DPYTHON_EXECUTABLE=${PYTHON_EXE} \
     -DGTSAM_INSTALL_MATLAB_TOOLBOX=${WITH_MATLAB} \
     -DGTSAM_BUILD_UNSTABLE:OPTION=${WITH_UNSTABLE} \
+    -DCMAKE_INSTALL_PREFIX=${install_path}
 
-    sudo make -j ${jobs} -C ${Buildpath} # |& tee MAKE_OUTPUT_LOG.txt
+    make -j ${jobs} -C ${build_path} # |& tee MAKE_OUTPUT_LOG.txt
     if [ "${ADD_CHECKS}" = true ]; then
-      sudo make check -j ${jobs} -C ${Buildpath} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
+      make check -j ${jobs} -C ${build_path} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
     fi
   else
-    sudo make check -j ${jobs} -C ${Buildpath} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
-    sudo make install -j ${jobs} -C ${Buildpath} # |& tee MAKE_INSTALL_OUTPUT_LOG.txt
+    make check -j ${jobs} -C ${build_path} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
+    make install -j ${jobs} -C ${build_path} # |& tee MAKE_INSTALL_OUTPUT_LOG.txt
   fi
 
   fi
@@ -195,10 +186,9 @@ if [ "${rebuild}" = true ]; then
 else
  # BUILDING FROM SCRATCH
   echo "Building with options..."
-  echo -e "\tBuildpath: $Buildpath"
+  echo -e "\tBuild path: ${build_path}"
+  echo -e "\tInstall path: ${install_path}"
   echo -e "\tJobs: $jobs"
-  echo -e "\tWith Dynamics Module: ${WITH_DYNAMICS_MODULE}"
-  echo -e "\tInstall Dependencies: ${install_deps}"
   echo -e "\tBuild Type: ${BUILD_TYPE}"
   echo -e "\tEnforced compile flags: ${ADD_CXX_FLAGS}"
   echo -e "\tPython wrapper build: ${WITH_PYTHON}"
@@ -209,53 +199,51 @@ else
   sleep 1
 
   # Install dependencies (should add check if already installed)
-  if [ "${install-deps}" = true ]; then
-    sudo apt update
-    sudo apt install libboost1.74-all-dev cmake libtbb-dev -y
-    #sudo apt install libboost-all-dev cmake libtbb-dev -y
-    sudo apt install gcc-11 g++-11
+  sudo apt update
+  #udo apt install libboost1.74-all-dev cmake libtbb-dev -y
+  #sudo apt install libboost-all-dev cmake libtbb-dev -y
+  sudo apt install gcc-11 g++-11
 
-    # NEED TO ADD IF "not installed" for the following:
-    #sudo apt-get install python3-pip -y 
-    #sudo apt-get install python-is-python3 -y
-    #pip install pyparsing numpy 
-    sudo apt-get install libeigen3-dev -y
-  fi
+  # NEED TO ADD IF "not installed" for the following:
+  #sudo apt-get install python3-pip -y 
+  #sudo apt-get install python-is-python3 -y
+  #pip install pyparsing numpy 
+  sudo apt-get install libeigen3-dev -y
 
 
-  if  [ -d $Buildpath ]; then
-      sudo rm -r ${Buildpath}/
+  if  [ -d $build_path ]; then
+      sudo rm -r ${build_path}/
   fi
 
   # Export path to use GCC 11.4 instead of >13.0
-  export CC=/usr/bin/gcc-11
-  export CXX=/usr/bin/g++-11
+  #export CC=/usr/bin/gcc-11
+  #export CXX=/usr/bin/g++-11
 
   # Generate makefiles with verbose output log
-  cmake  -B ${Buildpath} -S . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-          -DGTSAM_BUILD_UNSTABLE=${WITH_UNSTABLE} -DGTSAM_BUILD_CONVENIENCE_LIBRARIES=OFF \
+  cmake  -B ${build_path} -S . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+          -DGTSAM_BUILD_UNSTABLE=${WITH_UNSTABLE} \
           -DGTSAM_BUILD_PYTHON=${WITH_PYTHON} -DGTSAM_INSTALL_MATLAB_TOOLBOX=${WITH_MATLAB} \
           -DGTSAM_WITH_TBB=ON \
           -DGTSAM_WITH_EIGEN_MKL=OFF \
   	      -DGTSAM_UNSTABLE_BUILD_PYTHON=${WITH_PYTHON} \
-          -DGTSAM_WITH_DYNAMICS=${WITH_DYNAMICS_MODULE} \
           -DCMAKE_CXX_FLAGS=${ADD_CXX_FLAGS} \
           -DCMAKE_C_FLAGS=${ADD_CXX_FLAGS} \
           -DGTSAM_TANGENT_PREINTEGRATION=${USE_TANGENT_PREINTEGR} \
           -DGTSAM_POSE3_EXPMAP=${WITH_EXPMAP} \
-          -DGTSAM_ROT3_EXPMAP=${WITH_EXPMAP} 
+          -DGTSAM_ROT3_EXPMAP=${WITH_EXPMAP} \
+          -DCMAKE_INSTALL_PREFIX=${install_path}
   #        -DCMAKE_VERBOSE_MAKEFILE=ON . |& tee CMAKE_OUTPUT_LOG.txt .. 
 
   # Build and install (system-wide)
   if [ ${BUILD_TYPE} == "debug" ]; then
-    sudo make -j ${jobs} -C ${Buildpath} # |& tee MAKE_OUTPUT_LOG.txt
+    make -j ${jobs} -C ${build_path} # |& tee MAKE_OUTPUT_LOG.txt
     if [ "${ADD_CHECKS}" = true ]; then
-      sudo make check -j ${jobs} -C ${Buildpath} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
+      make check -j ${jobs} -C ${build_path} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
     fi
   else
-    sudo make -j ${jobs} -C ${Buildpath} # |& tee MAKE_OUTPUT_LOG.txt
-    sudo make check -j ${jobs} -C ${Buildpath} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
-    #sudo make install -j ${jobs} -C ${Buildpath} # |& tee MAKE_INSTALL_OUTPUT_LOG.txt
+    make -j ${jobs} -C ${build_path} # |& tee MAKE_OUTPUT_LOG.txt
+    make check -j ${jobs} -C ${build_path} # |& tee MAKE_CHECK_OUTPUT_LOG.txt
+    make install -j ${jobs} -C ${build_path} # |& tee MAKE_INSTALL_OUTPUT_LOG.txt
   fi
 
 fi
